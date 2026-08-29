3da6f265ddf6f96a2ef3889b519942f4308081a3
viusa <viusansantos@gmail.com>
2026-08-29 16:40:19 +0200

    automated grabbing each week instead of hardcoding, added comments so i can look back at this commit if i wonder what the sql statement means (if i were to remove this late r or change)

--- data_analysis.py
+++ data_analysis.py
@@ -3,7 +3,10 @@ import sqlite3
 conn = sqlite3.connect("nutrition.db")
 cursor = conn.cursor()
 
-cursor.execute("SELECT AVG(calories) FROM daily_summary_log WHERE date BETWEEN '2026-05-11' AND '2026-05-17'")
+#this will automate grouping by week so we dont need to hardcode each week
+#strftime my date from the column and rewrites into a pattern i give (her %Y being year and %W being week; so outcome would be 2026-33 for example as year and week number)
+#we save it as a variable week then we sort average as we would but replacing the hardcoded statement with our variable
+cursor.execute("SELECT strftime('%Y-%W', date) AS week, AVG(calories) FROM daily_summary_log GROUP BY week")
 rows = cursor.fetchall()
 
 for row in rows:
