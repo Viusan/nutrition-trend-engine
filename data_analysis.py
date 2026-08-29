1c771601a4f4c507603af805af21ee542be2f716
viusa <viusansantos@gmail.com>
2026-08-29 17:19:36 +0200

    less messy code and more organized by storing changes in varibles, and better SQL query

--- data_analysis.py
+++ data_analysis.py
@@ -6,8 +6,16 @@ cursor = conn.cursor()
 #this will automate grouping by week so we dont need to hardcode each week
 #strftime my date from the column and rewrites into a pattern i give (her %Y being year and %W being week; so outcome would be 2026-33 for example as year and week number)
 #we save it as a variable week then we sort average as we would but replacing the hardcoded statement with our variable
-cursor.execute("SELECT strftime('%Y-%W', date) AS week, AVG(calories) FROM daily_summary_log GROUP BY week")
+cursor.execute("SELECT strftime('%Y-%W', date) AS week, FLOOR(AVG(calories)), FLOOR(AVG(protein)), FLOOR(AVG(carbs)) FROM daily_summary_log GROUP BY week ORDER BY week")
+
+#gets most recent query executed through cursor
 rows = cursor.fetchall()
 
-for row in rows:
-    print(row)
+def percentage_change(new, old):
+    return int(((new-old)/old)*100)
+
+calorie_development = percentage_change(rows[-1][1], rows[-2][1])
+protein_development = percentage_change(rows[-1][2], rows[-2][2])
+carbs_development = percentage_change(rows[-1][2], rows[-2][2])
+
+print(f"Calorie change: {calorie_development}%\nProtein change: {protein_development}%\nCarbs change: {carbs_development}%")
